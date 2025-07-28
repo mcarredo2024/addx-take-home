abead0f66f96a3f45cb3020f4566616f840cdeb9
marvincarredo <marvin.carredo@gmail.com>
2025-07-28 22:02:06 +0800

    folder refactor

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "modules/vpc"
+}
+
+module "eks" {
+  source  = "modules/eks"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+}
+
+module "ecr" {
+  source = "modules/ecr"
+}
+
+module "rds" {
+  source      = "modules/rds"
+  vpc_id      = module.vpc.vpc_id
+  subnets     = module.vpc.private_subnets
+  db_user     = var.db_user
+  db_password = var.db_password
+}
+
+module "alb_ingress" {
+  source       = "modules/alb_ingress"
+  cluster_name = module.eks.cluster_name
+}
