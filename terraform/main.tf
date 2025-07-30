20fd6c7f957cc7ce812218b1024bba94619e8b01
marvincarredo <marvin.carredo@gmail.com>
2025-07-31 00:44:21 +0800

    fix eks version issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,21 +6,19 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name  = "nodejs-eks"
-  kubernetes_version = "1.33"
-
-
-  endpoint_public_access = true
-
+  name                      = "nodejs-eks"
+  kubernetes_version        = "1.29"
+  enable_irsa               = true
+  endpoint_public_access    = true
   enable_cluster_creator_admin_permissions = true
+
+  vpc_id   = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   compute_config = {
     enabled    = true
     node_pools = ["general-purpose"]
   }
-
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
 }
 
 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = "~> 2.11"
     }
   }
 
