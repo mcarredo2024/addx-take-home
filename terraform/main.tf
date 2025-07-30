b2226d119e98aea4d67f05c584eb6249c965a2eb
marvincarredo <marvin.carredo@gmail.com>
2025-07-31 00:38:17 +0800

    fix eks version issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,24 +4,23 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.17.2"
+  version = "~> 21.0"
 
-  cluster_name    = "nodejs-eks"
-  cluster_version = "1.29"
+  name  = "nodejs-eks"
+  kubernetes_version = "1.33"
+
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
   vpc_id          = module.vpc.vpc_id
   subnets         = module.vpc.private_subnets
-
-  enable_irsa = true
-
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
-      subnets          = module.vpc.private_subnets
-    }
-  }
 }
 
 
